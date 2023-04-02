559968121958c72899118be0fd060135d3920288
Aftahiislam007 <aftahiislam2016@gmail.com>
2023-04-03 01:56:15 +0600

    first_commit

---
+++ todo_api/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class TodoApiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "todo_api"
+    verbose_name = 'A Much Better Name'
