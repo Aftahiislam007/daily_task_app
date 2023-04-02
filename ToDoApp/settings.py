2fff316e40e4df60b2eb426f894633ca3f0e4262
Aftahiislam007 <aftahiislam2016@gmail.com>
2023-04-03 03:10:26 +0600

    fifth_commit

--- ToDoApp/settings.py
+++ ToDoApp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-1t4o91pb+35e0nuu#0*!#+@-*a20#^d%ime=l!dxr3kh^^o1*$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['daily-task007.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
