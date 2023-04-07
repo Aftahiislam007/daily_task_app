2357b16cc12cd304af766ed1d1eadd4e947004ba
Aftahiislam007 <aftahiislam2016@gmail.com>
2023-04-07 16:55:35 +0600

    eleventh_commit

--- ToDoApp/settings.py
+++ ToDoApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-1t4o91pb+35e0nuu#0*!#+@-*a20#^d%ime=l!dxr3kh^^o1*$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['daily-task-app007.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
