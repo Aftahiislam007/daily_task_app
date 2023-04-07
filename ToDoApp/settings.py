4c6a6cf74f67ae9c9244a0db9db45eacb5dbd243
Aftahiislam007 <aftahiislam2016@gmail.com>
2023-04-07 16:58:49 +0600

    twelveth_commit

--- ToDoApp/settings.py
+++ ToDoApp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-1t4o91pb+35e0nuu#0*!#+@-*a20#^d%ime=l!dxr3kh^^o1*$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
